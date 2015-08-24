5ee4b2a4189e949f1215302ead7c7b4a6a72811e
David Brockman Smoliansky <david@springworks.se>
2015-08-24 10:05:11 +0200

    Fix linter warning

--- index.js
+++ index.js
@@ -21,16 +21,13 @@ var internals = {};
  * @throws {Error} If config object is invalid.
  */
 exports.create = function(iterator, config, logger) {
-  var sqs;
-
-  config = validator.validateSchema(config, config_schema);
-
-  sqs = new SQS({
-    region: config.region,
-    apiVersion: config.api_version
+  var validated_config = validator.validateSchema(config, config_schema);
+  var sqs = new SQS({
+    region: validated_config.region,
+    apiVersion: validated_config.api_version
   });
 
-  return internals.create(sqs, iterator, config, logger);
+  return internals.create(sqs, iterator, validated_config, logger);
 };
 
 
@@ -43,38 +40,32 @@ exports.create = function(iterator, config, logger) {
  * @return {Object}              See above.
  */
 internals.create = function(sqs, iterator, config, logger) {
-  var sqs_handler,
-      message_queue,
-      message_processing,
-      message_capture,
-      sqs_timeout_handler,
-      sqs_processor,
-      emitter = new EventEmitter();
+  var emitter = new EventEmitter();
 
-  sqs_handler = require('./lib/queue/sqs-handler.js').create(
+  var sqs_handler = require('./lib/queue/sqs-handler.js').create(
       sqs,
       config,
       logger);
 
-  message_queue = require('./lib/queue/message-queue.js').create(
+  var message_queue = require('./lib/queue/message-queue.js').create(
       sqs_handler,
       logger);
 
-  message_processing = require('./lib/processing/message-processing.js').create(
+  var message_processing = require('./lib/processing/message-processing.js').create(
       iterator,
       message_queue,
       logger);
 
-  message_capture = require('./lib/message-capture.js').create(
+  var message_capture = require('./lib/message-capture.js').create(
       message_queue,
       message_processing,
       logger);
 
-  sqs_timeout_handler = require('./lib/sqs-timeout-handler.js').create(
+  var sqs_timeout_handler = require('./lib/sqs-timeout-handler.js').create(
       config,
       logger);
 
-  sqs_processor = require('./lib/sqs-processor.js').create(
+  var sqs_processor = require('./lib/sqs-processor.js').create(
       message_capture,
       sqs_timeout_handler,
       emitter,
